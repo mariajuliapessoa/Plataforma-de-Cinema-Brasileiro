1e1ed4eddf7740f389987daa7a3a3647c5b4063b
vinivent <vini.ventura999@gmail.com>
2025-03-21 20:02:19 -0300

    feat: adição projeto spring boot

---
+++ bracine/src/main/java/com/cesar/bracine/BracineApplication.java
@@ -0,0 +1,13 @@
+package com.cesar.bracine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BracineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BracineApplication.class, args);
+	}
+
+}
---
+++ bracine/src/test/java/com/cesar/bracine/BracineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cesar.bracine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BracineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
