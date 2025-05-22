e58a1a9613800f0f5eb0e0d898b906697b01f598
vinivent <vini.ventura999@gmail.com>
2025-05-21 21:28:20 -0300

    feat: configuracao do .env

--- backend/src/main/java/com/cesar/bracine/BracineApplication.java
+++ backend/src/main/java/com/cesar/bracine/BracineApplication.java
@@ -1,13 +1,21 @@
 package com.cesar.bracine;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BracineApplication {
+    public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.configure()
+                              .directory("./backend")
+                              .load();
 
-	public static void main(String[] args) {
-		SpringApplication.run(BracineApplication.class, args);
-	}
+        dotenv.entries().forEach(entry ->
+            System.setProperty(entry.getKey(), entry.getValue())
+        );
 
+        // Agora inicia o Spring
+        SpringApplication.run(BracineApplication.class, args);
+    }
 }
