21b84139498cadd90a2989c886c931f9cae2ba50
Vinícius Ventura <vini.ventura999@gmail.com>
2025-05-29 02:47:44 -0300

    fix: import dot env

--- backend/src/main/java/com/cesar/bracine/BracineApplication.java
+++ backend/src/main/java/com/cesar/bracine/BracineApplication.java
@@ -2,10 +2,18 @@ package com.cesar.bracine;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class BracineApplication {
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.configure()
+                              .directory(".")
+                              .load();
+
+        dotenv.entries().forEach(entry ->
+            System.setProperty(entry.getKey(), entry.getValue())
+        );
         SpringApplication.run(BracineApplication.class, args);
     }
 }
